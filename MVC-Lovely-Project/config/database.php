d85ea76e4e27a1f986e1cbcfd87d5c2cac432de1
1redjasper <homaaty@gmail.com>
2022-04-12 21:31:59 +0430

    Update database.php

--- MVC-Lovely-Project/config/database.php
+++ MVC-Lovely-Project/config/database.php
@@ -3,6 +3,6 @@
 return [
     'DBHOST' => 'https://1redjasper.github.io/irani-price/',
     'DBNAME' => 'new_mvc',
-    'DBUSERNAME' => '1redjasper',
+    'DBUSERNAME' => 'homaaty@gmail.com',
     'DBPASSWORD' => '12345ahAH',
 ];
